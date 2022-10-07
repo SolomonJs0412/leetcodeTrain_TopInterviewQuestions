6bc121d99882a5f3c437b64f0a84a3811cc16f34
solomonjs0412 <vothuykg123@gmail.com>
2022-10-08 00:20:35 +0700

    done

--- intersectionTwoArrays.js
+++ intersectionTwoArrays.js
@@ -1,43 +1,11 @@
-/**
- * @param {number[]} num1
- * @param {number[]} num2
- * @return {number[]}
- */
 var intersect = function (num1, num2) {
-    let ins = [];
-    for (let i = 0; i < num1.length; i++) {
-        for (let j = 0; j < num2.length; j++) {
-            if (num1[i] === num2[j]) {
-                for (let z = 0; z < ins.length; z++) {
-                    if (num1[i] !== ins[z]) {
-                        ins.push(num1[i]);
-                    }
-                }
-            }
+    let sol = [];
+    for (let i = 0; i < num2.length; i++) {
+        let index = num1.indexOf(num2[i]);
+        if (index >= 0) {
+            sol.push(num2[i]);
+            num1.splice(index, 1);
         }
     }
-    for (let i = 0; i < ins.length; i++) {
-        console.log(ins[i]);
-    }
-
-    return ins
-
-    // for (let i = 0; i < num1.length; i++) {
-    //     for (let j = 0; j < num2.length; j++) {
-    //         if (num1[i] === num2[j]) {
-    //             ins.push(num1[i]);
-    //             num1.slice(num1[i])
-    //         }
-    //     }
-    // }
-
-    // if ()
-
-    //     for (let i = 0; i < ins.length; i++) {
-    //         console.log(ins[i]);
-    //     };
+    return sol.sort();
 }
-let abc = [1, 2, 2, 1]
-let xyz = [2, 2]
-
-console.log(intersect(abc, xyz));
