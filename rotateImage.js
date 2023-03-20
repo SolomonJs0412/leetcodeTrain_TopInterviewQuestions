ca262f105f4a2c8f909e7c829df85db04bd0e97b
ashley <vothuykg123@gmail.com>
2023-03-20 19:01:00 +0700

    update

---
+++ rotateImage.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i; j < matrix[i].length; j) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length / 2; j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[i][matrix.length - 1 - j];
+            matrix[i][matrix.length - 1 - j] = temp;
+        }
+    }
+    return matrix;
+};
+
+let matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+
+console.log(rotate(matrix));
